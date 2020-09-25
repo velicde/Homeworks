5e35400c683a56d30463663fd77ca01d34b6566c
Deivis <Deivis@DESKTOP-8UU57L0>
2020-09-25 17:45:36 +0300

    2020-09-25 17:40

---
+++ src/lt/vtmc/deividas/pd5/PD5_1.java
@@ -0,0 +1,10 @@
+package lt.vtmc.deividas.pd5;
+
+public class PD5_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/lt/vtmc/deividas/pd6/PD6_1.java
@@ -0,0 +1,10 @@
+package lt.vtmc.deividas.pd6;
+
+public class PD6_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
